8b2f216cf296ade0b4ae2e9724411e5f932f3ae2
İbrahim Sağlam <saglam.354270@gmail.com>
2023-12-05 23:23:52 +0300

    SpringAnnotations

---
+++ src/main/java/com/tpe/SpringBootB91SsgApplication.java
@@ -0,0 +1,13 @@
+package com.tpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootB91SsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootB91SsgApplication.class, args);
+	}
+
+}
